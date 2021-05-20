15206d13d8ed7a236e71ada74f920d873cb50405
Neeraj Tiwari <nt98924@gmail.com>
2021-05-20 12:00:36 +0530

    bootstrap test

---
+++ js/basic.js
@@ -0,0 +1,13 @@
+let num1 = prompt('Enter value of num1'),
+	num2 = prompt('Enter value of num2');
+
+if (num1 > num2) console.log('true condition');
+else console.log(`${num2} is bigger than ${num1}!!`);
+
+let num5 = 10;
+if (num5 > 130) console.log('num5 is greater than 130');
+else if (num5 == 130) console.log('num5 is 180');
+else console.log('sorry no match');
+
+if (num1 > 80 && num2 < 60) console.log('num1 is greater than 80, num2 is smaller than 60');
+else console.log(`${num2} is bigger than ${num1}!!`);
