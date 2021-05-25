2e7a96b976a6eeb946f6a8eed147214a91e9e3bb
Neeraj Tiwari <nt98924@gmail.com>
2021-05-25 19:30:10 +0530

    js doubts

--- js/exersise2.js
+++
@@ -1,7 +0,0 @@
-let firstName = `Neeraj`;
-undefined
-let lastName = `Tiwari`
-undefined
-console.log(`firstName + lastName`)
-firstName+lastName
-"NeerajTiwari"
--- js/exersise4.js
+++ js/exersise4.js
@@ -1,2 +1,6 @@
-let oddNum = 12;
-let evenNum = 11;
+let num1 = `5`;
+let num2 = `6`;
+if(num1%2!=0) console.log(`odd`)
+if(num2%2==0) console.log(`even`)
+ 
+
