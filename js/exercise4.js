bbac90fff826c4d67aa49c367b1297f45ae26105
Neeraj Tiwari <nt98924@gmail.com>
2021-05-26 20:15:22 +0530

    js doubts

---
+++ js/exercise4.js
@@ -0,0 +1,14 @@
+let myTest = 734740904;
+if(734740904==0) console.log(`even number`)
+else console.log(`odd number`)
+
+
+
+let num1 = 4,
+let num2 = '6',
+let num3 = 'abc',
+let num4 = 32,
+let num5 = 'last'
+
+ 
+
--- js/exersise4.js
+++
@@ -1,6 +0,0 @@
-let num1 = `5`;
-let num2 = `6`;
-if(num1%2!=0) console.log(`odd`)
-if(num2%2==0) console.log(`even`)
- 
-
