01bfb30ab4a0e9347af425822188579d5c576c60
Neeraj Tiwari <nt98924@gmail.com>
2021-06-06 19:50:58 +0530

    added arrey

---
+++ js/Arreys.js
@@ -0,0 +1,9 @@
+let myFamily = ['main','Mummy','papa','bhai','bahan','dadi','chacha','chachi',]
+// myFamily[7]
+"chachi"
+ myFamily[7].length
+// 6
+myFamily.push('vishal')
+// 9
+myFamily
+// (9) ["main", "Mummy", "papa", "bhai", "bahan", "dadi", "chacha", "chachi", "vishal"]
