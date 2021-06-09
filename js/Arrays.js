d868dc8e1f949f03cc1c47cb50d9adfe24cb4af4
Neeraj Tiwari <nt98924@gmail.com>
2021-06-09 06:59:32 +0530

    added new practice file

---
+++ js/Arrays.js
@@ -0,0 +1,12 @@
+let myFamily = ['main','Mummy','papa','bhai','bahan','dadi','chacha','chachi']
+//  myFamily[7]
+// "chachi"
+//  myFamily[7].length
+// 6
+// // myFamily.push('vishal')
+//  9
+// myFamily
+//  (9) ["main", "Mummy", "papa", "bhai", "bahan", "dadi", "chacha", "chachi", "vishal"]
+
+ let myFirstName = 'Neeraj', mySurName='Tiwari'
+ console.log(myFirstName + mySurName);
--- js/Arreys.js
+++
@@ -1,9 +0,0 @@
-let myFamily = ['main','Mummy','papa','bhai','bahan','dadi','chacha','chachi',]
-// myFamily[7]
-"chachi"
- myFamily[7].length
-// 6
-myFamily.push('vishal')
-// 9
-myFamily
-// (9) ["main", "Mummy", "papa", "bhai", "bahan", "dadi", "chacha", "chachi", "vishal"]
