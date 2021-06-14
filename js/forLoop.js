68bfd8ae85995a606e6f37be61b6f83f148b50c8
Neeraj Tiwari <nt98924@gmail.com>
2021-06-14 15:32:37 +0530

    for loop

---
+++ js/forLoop.js
@@ -0,0 +1,6 @@
+let min = 0 ,
+max = 100;
+for(let i = min; i < max; i++){
+
+    console.log('i am at record number ${i}')
+}
--- js/whileloop.js
+++ js/whileloop.js
@@ -1,6 +1,11 @@
-let alto = 50, BMW = 90;
-while(alto<BMW){
-    (alto++)
-    console.log(alto)
+//  let alto = 50, BMW = 90;
+//  while(alto<BMW){
+//    (alto++)
+//      console.log(alto)
 
+//  }
+
+const secret = 'folk singer';
+while {prompt('what do u want to be ?' )!=secret}{
+    console.log('congo.. you are now !${secret}' );
 }
