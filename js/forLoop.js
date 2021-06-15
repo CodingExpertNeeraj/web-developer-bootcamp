49244d1db551212702e5c8da5766cd9db56260f3
Neeraj Tiwari <nt98924@gmail.com>
2021-06-15 21:06:03 +0530

    todo project

--- js/forLoop.js
+++ js/forLoop.js
@@ -2,5 +2,5 @@ let min = 0 ,
 max = 100;
 for(let i = min; i < max; i++){
 
-    console.log('i am at record number ${i}')
+    console.log(`i am at record number ${i}`)
 }
---
+++ js/object.js
@@ -0,0 +1,17 @@
+let myVar = 10;
+
+function myFirstFunction() {
+	let myVar2 = 20;
+	console.log(`This is my First function with ${myVar}!!!`);
+}
+
+function myFirstFunction2() {
+	let myVar = 30;
+	console.log(`This is my First function with ${myVar}!!!`);
+}
+
+// console.log(myVar2);
+
+// myFirstFunction();
+
+// myFirstFunction2();
