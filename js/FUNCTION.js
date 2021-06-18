d0168e8ed95b1bc9644abe522a94a67a1adf793f
Neeraj Tiwari <nt98924@gmail.com>
2021-06-18 20:18:09 +0530

    function

---
+++ js/FUNCTION.js
@@ -0,0 +1,51 @@
+// let myVar = 10;
+
+// function myFirstFunction() {
+// // 	let myVar2 = 20;
+// 	console.log(`This is my First function with !!!`);
+//  }
+
+// function myFirstFunction2() {
+// 	let myVar = 30;
+// 	console.log(`This is my First function with ${myVar}!!!`);
+// }
+
+//  console.log(myVar2);
+
+//  myFirstFunction();
+//  console.log (myVar);
+
+// myFirstFunction2();
+
+// function  myFirstFunction{} 
+//     console.log(`this is neerajs function!!!`);
+
+// myFirstFunction
+
+// ${myVar}
+
+function ourPrimeMinister(){
+
+	
+}
+console.log('mr.Narendra modi is our prime minister')
+ourPrimeMinister();
+
+function aboutGuruji(){
+	
+}
+// aboutGuruji();
+console.log('my guruji name is mr. rachit rastogi he lives in america')
+
+
+
+function printHeart(){
+    console.log (`<3`);
+}
+printHeart();
+
+
+ printHeart()
+ printHeart()
+ printHeart()
+ printHeart()
---
+++ js/todo.js
@@ -0,0 +1,5 @@
+let input = prompt("what would you like to do?")
+ while (input !== 'quit' && input !== 'q'){
+     input = ("what would you like to do?")
+ }
+ console.log("OK QUIT THE APP!")
