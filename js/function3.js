3c73be5a47c657fb1ec1dbe2dda9bfacac5610c2
Neeraj Tiwari <nt98924@gmail.com>
2021-06-23 12:30:35 +0530

    3 way of writing function

---
+++ js/function3.js
@@ -0,0 +1,22 @@
+ //One way of write function
+
+function printMyName (name) {
+
+    console.log(name);
+}
+printMyName('Neeraj');
+
+
+ //second way to write function
+ 
+ const printMyNameAnotherWay = function(name){
+     console.log(name);
+ }
+ printMyNameAnotherWay('Neeraj');
+
+ //Third way to write function
+
+ const printMyNameInSMARTWay = (name) =>{
+     console.log(name);
+ }
+ printMyNameInSMARTWay('Neeraj');
