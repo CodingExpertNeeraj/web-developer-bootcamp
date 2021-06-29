d45dd06bbe15ec8abed43acbc010722bc622cc9b
Neeraj Tiwari <nt98924@gmail.com>
2021-06-29 21:07:10 +0530

    new function

--- js/secret.js
+++ js/secret.js
@@ -1,9 +1,28 @@
-let i = 0; j = 5;
+// let i = 0; j = 5;
 
-for (let i = 0; i <= j ; i++){
-    console.log ('Da ,ba , dee, da, ba, daa'); 
-     console.log (`${i}`);
-    console.log('This is neeraj,s loop');
-    console.log('ka smjhe ? kuch aaya smjh mai ?');
-    console.log(`lol`);
-}
+// for (let i = 0; i <= j ; i++){
+//     console.log ('Da ,ba , dee, da, ba, daa'); 
+//      console.log (`${i}`);
+//     console.log('This is neeraj,s loop');
+//     console.log('ka smjhe ? kuch aaya smjh mai ?');
+//     console.log(`lol`);
+// }
+//-------------------------------------------
+// let x = 4, y = 3;
+// let square = function(x, y){
+//     return 4* 4, 3 * 3;
+//     console.log()
+// };
+//----------------------------------
+// let aboutNeeraj  = {
+
+//     firstName  : 'Neeraj',
+//     lastName  : 'Tiwari',
+//     officialName:'Nick',
+//     age:21,
+//     wantsToBeCome:'fullstachdevloper',
+//     bestFriend:'Rahul',
+// };
+//----------------------------------------------------
+const multiplyBy5 = (Number) => Number * 8 + 4;
+console.log(multiplyBy5(7));
