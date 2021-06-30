c736cef809cb1326250a917c8280075a9e26520a
Neeraj Tiwari <nt98924@gmail.com>
2021-06-30 21:38:32 +0530

    try-catch

---
+++ js/tryCatch.js
@@ -0,0 +1,47 @@
+// function printNumberAfterDivision(number, divisor) {
+// 	return number / divisor;
+// }
+
+// const printNumberAfterDivision = (number, divisor) => {
+// 	try {
+// 		var value = number / divisor;
+// 	} catch (e) {
+// 		console.log(e);
+// 	}
+// 	return value;
+// };
+
+// try {
+// 	hello.toupperCase();
+// } catch (e) {
+// 	console.log(e);
+// }
+// // hello.toupperCase();
+
+// console.log(printNumberAfterDivision(20, 10));
+
+// const upperCaseConversion = (input) => {};
+
+// upperCaseConversion('saurabh');
+
+function upperCaseConversion(input) {
+	try {
+        return input.toUpperCase();
+        
+	} catch (e) {
+		console.log(e);
+	} finally {
+		console.log('value Entered and checked!!');
+	}
+
+	// return input.toUpperCase();
+}
+
+console.log(upperCaseConversion('saurabh'));
+console.log(upperCaseConversion('neeraj'));
+console.log(upperCaseConversion(1));
+console.log(upperCaseConversion('kanchan'));
+console.log(upperCaseConversion('neeraj'));
+console.log(upperCaseConversion('neeraj'));
+console.log(upperCaseConversion('neeraj'));
+console.log(upperCaseConversion(100));
