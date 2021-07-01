6bba36ae751c78a1da536ed9a0bc08c566ed51f6
Neeraj Tiwari <nt98924@gmail.com>
2021-07-01 15:16:14 +0530

    try-catch practice with own

--- js/tryCatch.js
+++ js/tryCatch.js
@@ -1,47 +1,64 @@
-// function printNumberAfterDivision(number, divisor) {
-// 	return number / divisor;
-// }
+// // function printNumberAfterDivision(number, divisor) {
+// // 	return number / divisor;
+// // }
 
-// const printNumberAfterDivision = (number, divisor) => {
+// // const printNumberAfterDivision = (number, divisor) => {
+// // 	try {
+// // 		var value = number / divisor;
+// // 	} catch (e) {
+// // 		console.log(e);
+// // 	}
+// // 	return value;
+// // };
+
+// // try {
+// // 	hello.toupperCase();
+// // } catch (e) {
+// // 	console.log(e);
+// // }
+// // // hello.toupperCase();
+
+// // console.log(printNumberAfterDivision(20, 10));
+
+// // const upperCaseConversion = (input) => {};
+
+// // upperCaseConversion('saurabh');
+
+// function upperCaseConversion(input) {
 // 	try {
-// 		var value = number / divisor;
+//         return input.toUpperCase();
+        
 // 	} catch (e) {
 // 		console.log(e);
+// 	} finally {
+// 		console.log('value Entered and checked!!');
 // 	}
-// 	return value;
-// };
 
-// try {
-// 	hello.toupperCase();
-// } catch (e) {
-// 	console.log(e);
+// 	// return input.toUpperCase();
 // }
-// // hello.toupperCase();
 
-// console.log(printNumberAfterDivision(20, 10));
+// console.log(upperCaseConversion('saurabh'));
+// console.log(upperCaseConversion('neeraj'));
+// console.log(upperCaseConversion(1));
+// console.log(upperCaseConversion('kanchan'));
+// console.log(upperCaseConversion('neeraj'));
+// console.log(upperCaseConversion('neeraj'));
+// console.log(upperCaseConversion('neeraj'));
+// console.log(upperCaseConversion(100));
 
-// const upperCaseConversion = (input) => {};
+//-------------------------------
 
-// upperCaseConversion('saurabh');
+//hello.toUpperCase()
 
-function upperCaseConversion(input) {
-	try {
-        return input.toUpperCase();
-        
-	} catch (e) {
-		console.log(e);
-	} finally {
-		console.log('value Entered and checked!!');
-	}
+try{
+	hello.toUpperCase();
+ } catch(e){
+	console.log(e);
+};
 
-	// return input.toUpperCase();
-}
 
-console.log(upperCaseConversion('saurabh'));
-console.log(upperCaseConversion('neeraj'));
-console.log(upperCaseConversion(1));
-console.log(upperCaseConversion('kanchan'));
-console.log(upperCaseConversion('neeraj'));
-console.log(upperCaseConversion('neeraj'));
-console.log(upperCaseConversion('neeraj'));
-console.log(upperCaseConversion(100));
+ try{
+ myParagraph.toUpperCase();
+  } catch(e) {
+ 	 console.log(e);
+  }
