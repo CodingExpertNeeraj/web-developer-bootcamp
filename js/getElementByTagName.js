07e08f1edfc64a5e91a1b6660c43c2dd04c614bf
Neeraj Tiwari <nt98924@gmail.com>
2022-05-02 09:18:03 +0530

    get element by tag name practice

---
+++ js/getElementByTagName.js
@@ -0,0 +1,11 @@
+
+
+document.getElementsByTagName('h1')[0].style.background="red";
+
+document.getElementsByTagName('b')[1].style.background="yellow";
+document.getElementsByTagName('p')[0].style.fontSize="50px";
+document.getElementsByTagName('p')[0].style.color="red";
+document.getElementsByTagName('div')[0].style.textAlign="center";
+document.getElementsByTagName('div')[0].style.fontSize="50px";
+
+document.write("Hello world")
